0428cd270eed89571fc53ea987773fd3bececd37
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-28 11:20:24 +0100

    initial commit

---
+++ downloads/g5k-netmonitor-image/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location:https://unice-my.sharepoint.com/:f:/g/personal/alessio_pagliari_unice_fr/Eg_JxkaytmRErAP5fUMLm70BrAMdAs_sHptMe27AvGx6Pw?e=XhqInH");
+die();
+?>
---
+++ downloads/g5k-storm-image/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location:https://unice-my.sharepoint.com/:f:/g/personal/alessio_pagliari_unice_fr/EhjoAn93YeFIimInroGWveIBuAx6YiUkedtbwOkADe4N0A?e=TGvLXx");
+die();
+?>
